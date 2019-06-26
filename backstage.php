56c069dc842a11e3a1fd50a64d848389c8fea367
chang <1834669416@qq.com>
2019-06-26 09:35:35 +0800

    finally edition

--- backstage.php
+++ backstage.php
@@ -23,15 +23,8 @@ userPassword varchar(20) not null
 if($conn->query($sql)==true){
     echo "create table successful";
 }
-if($_GET["mainuserPassword"]!=$_GET["subuserPassword"]){
-    die("请您确认密码");
-}
 //插入数据
-$name=empty($_GET["userName"])?die("请输入您的用户名"):$_GET["userName"];
-$email=empty($_GET["userEmail"])?die("请输入您要绑定的邮箱"):$_GET["userEmail"];
-$identify=empty($_GET["userIdentify"])?die("请您进行身份认证"):$_GET["userIdentify"];
-$school=empty($_GET["userSchool"])?die("选择您的学校"):$_GET["userSchool"];
-$password=empty($_GET["mainuserPassword"])?die("输入您的登陆密码"):$_GET["mainuserPassword"];
+
 /*$sql="insert into information(userName,userEmail,userIdentical,userSchool,userPassword)
 values ('$name','$email','$identify','$school','$password')";
 if($conn->query($sql)==true){
@@ -39,33 +32,47 @@ if($conn->query($sql)==true){
 }else{
     echo "faile".$conn->error;
 }*/
+$name=empty($_GET["userName"])?die("请输入您的用户名"):$_GET["userName"];
+$password=empty($_GET["mainuserPassword"])?die("输入您的登陆密码"):$_GET["mainuserPassword"];
+if(isset($_COOKIE["$name"])){//如果该用户已经注册
+echo "您已经注册请";
+echo " <a href=\"denglu.html\">登陆</a>";
 
-if(isset($_COOKIE["$name"])&isset($_SESSION["$name"])){//如果该用户已经注册
-    $sql="select userPassword from information where userName='name'";
+    /*die(var_dump($sql));
     $passwordValid=$conn->query($sql);//从数据库取出密码
-    //echo $passwordValid;
-    if($passwordValid!=$password){//验证密码
+    //echo $passwordValid;*/
+   /* if($passwordValid!=$password){//验证密码
         echo "密码输入错误";
     }else{
         $_SESSION['$name']=$_SESSION['$name']+1;
         echo "欢迎回来".$name."这是您第".$_SESSION['$name']."次访问";
-    }
+    }*/
 }else{//没注册过
-    setcookie("$name", "$name", time()+3600);//cookie
-    $_SESSION['$name']=1;
-    $sql="insert into information(userName,userEmail,userIdentical,userSchool,userPassword)
+    $name=empty($_GET["userName"])?die("请输入您的用户名"):$_GET["userName"];
+    $email=empty($_GET["userEmail"])?die("请输入您要绑定的邮箱"):$_GET["userEmail"];
+    $identify=empty($_GET["userIdentify"])?die("请您进行身份认证"):$_GET["userIdentify"];
+    $school=empty($_GET["userSchool"])?die("选择您的学校"):$_GET["userSchool"];
+    $password=empty($_GET["mainuserPassword"])?die("输入您的登陆密码"):$_GET["mainuserPassword"];
+    if($_GET["mainuserPassword"]!=$_GET["subuserPassword"]){
+        die("请您确认密码");
+    }else{
+        setcookie("$name", "$name", time()+3600);//cookie
+        $_SESSION['$name']=1;
+        $sql="insert into information(userName,userEmail,userIdentical,userSchool,userPassword)
 values ('$name','$email','$identify','$school','$password')";//存储该用户信息
 
-    if($conn->query($sql)==true){
-        echo "注册成功","这是您第1次访问";
-        ?>
-                <form action="register.html" method="get">
-         <input type="submit" value="退出登陆" name="back"/>
-        </form>
+        if($conn->query($sql)==true){
+            echo "注册成功","这是您第1次访问";
+            ?>
+            <form action="register.html" method="get">
+                <input type="submit" value="退出登陆" name="back"/>
+            </form>
 
-  <?php  }else{
-        echo "注册失败请检查".$conn->error.PHP_EOL;
+        <?php  }else{
+            echo "注册失败请检查".$conn->error.PHP_EOL;
+        }
     }
+
 }
 /*<!DOCTYPE html>
 <html>
